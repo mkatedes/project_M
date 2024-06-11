5213a36978305442eee3bfbe0d45c53ee045eb51
unknown <matteo.geffroy@gmail.com>
2024-06-11 19:22:27 +0200

    date

--- gitbs.sh
+++ gitbs.sh
@@ -1,7 +1,7 @@
+cd C:\Users\olaka\Documents\project_M
 git checkout b
 date=$(date '+%Y-%m-%d')
 date>>aa
 git add .
-git commit -m "a"
+git commit -m date
 git push --set-upstream origin b
-sleep 10s
